810ab4230f61bd6482915876995c0166b1cd8ae3
Robin Scheibler <r-scheibler@zf.jp.nec.com>
2012-02-16 18:13:08 +0900

    Fixed Yule-Walker to operate on columns of a matrix. Modified autoregressive spectrum to window the data before computations. Performance much improved.

--- audiotools/spectral.py
+++ audiotools/spectral.py
@@ -4,6 +4,9 @@ import numpy as np
 from windows import *
 from scipy.signal import correlate as correlate
 
+from transforms import *
+from windows import *
+
 # DCT spectrogram
 def spectrogram(x, N, L, D, transform=np.fft.fft, win=hann):
 
@@ -102,34 +105,50 @@ def margortceps(Y, L, D, transform=np.fft.ifft, win=hann):
 
 
 # Solves the Yule-Walker equations and returns the linear prediction coefficients using Levinson-Durbin algorithm
+# If X is a matrix, the algorithm operates on the columns of the matrix
 def yule_walker(X, M):
 
+  N = X.shape[0]
   if (len(X.shape) > 1):
-    print 'Warning: multidimensional arrays not supported yet. May (will) fail.'
-
-  N = len(X)
+    F = X.shape[1]
+  else:
+    F = 1
   
   A = np.zeros(M+1)
   E = 0
 
-  R = correlate(X, X)/N
-  R = R[N-1:]
+  # fft based columnwise correlation
+  R = np.zeros((N,1))
+  R = np.real(np.fft.ifft(np.abs(np.fft.fft(X,n=2*N-1,axis=0))**2,n=None,axis=0))[:N,]/N
 
-  a = np.zeros(M)
-  e = R[0]
+  if (F == 1):
+    a = np.zeros(M)
+  else:
+    a = np.zeros((M,F))
+
+  e = R[0,]
 
   # first loop
-  k = R[1]/e
-  a[0] = k
+  k = R[1,]/e
+  a[0,] = k
   e = e*(1 - k**2)
 
   # rest of the loop
   for l in range(2,M+1):
-    k = (R[l] - (a[0:l-1]*R[l-1:0:-1]).sum())/e
-    a[0:l] = np.hstack((a[0:l-1] , 0)) - k*np.hstack((a[l-2::-1] , -1))
+    k = (R[l,] - (a[0:l-1,]*R[l-1:0:-1,]).sum(axis=0))/e
+    if (F == 1):
+      a[0:l] = np.hstack((a[0:l-1] , 0)) - k*np.hstack((a[l-2::-1] , -1))
+    else:
+      a[0:l,] = np.vstack((a[0:l-1,] , np.zeros((1,F)))) - k*np.vstack((a[l-2::-1,] , -np.ones((1,F))))
     e = e*(1 - k**2)
 
-  A = np.hstack((1, -a))
+  if (F == 1):
+    A = np.hstack((1, -a))
+  else:
+    A = np.vstack((np.ones((1,F)), -a))
+
+  if (F == 1):
+    A = A.T
 
   return A, e
 
@@ -150,7 +169,7 @@ def auto_regressive_spectrum(x, M, f):
 
 
 # AR spectrogram estimate
-def ar_spectrogram(x, L, D, M, f):
+def autoregressive_spectrogram(x, L, D, M, f, window=hann):
 
   # pad if necessary
   if (len(x)%L != 0):
@@ -174,19 +193,22 @@ def ar_spectrogram(x, L, D, M, f):
     Y = X
 
   Z = np.zeros((len(f), Y.shape[1]), dtype=float)
-  AR = np.zeros((M+1, Y.shape[1]), dtype=float)
-  E = np.zeros(Y.shape[1], dtype=float)
+
+  # precomputation of complex exponentials for later use
+  I = np.outer(np.arange(0,M+1), f)
+  EXP = np.exp(-2.j*np.pi*I)
+
+  # window function
+  W = np.tile(window(Y.shape[0]), (Y.shape[1], 1)).T
+
+  # compute Yule-Walker coefficients (on columns)
+  AR, E = yule_walker(W*Y, M)
 
   # compute DCT
   for i in range(0,F):
-    AR[:,i], E[i] = yule_walker(x, M)
-
     N = len(f)
-
-    I = np.outer(np.arange(0,M+1), f)
     A = np.tile(AR[:,i], (N, 1)).T
-
-    Z[:,i] =  E[i]/np.abs((A*np.exp(-2.j*np.pi*I)).sum(axis=0))**2
+    Z[:,i] =  E[i]/np.abs((A*EXP).sum(axis=0))**2
 
   return Z, AR, E
 
